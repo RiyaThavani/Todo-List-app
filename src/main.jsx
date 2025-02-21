e10e55becd1ba798caf0ac29793e8cf30a657efd
rahul thavani <rahulthavani778@gmail.com>
2025-02-21 20:21:57 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -22,3 +22,5 @@ createRoot(document.getElementById('root')).render(
 // 1) git add .
 // 2) git commit -m "git commands added"
 // 3) git push origin master 
+
+// git clone https://github.com/RiyaThavani/TodoList.git
